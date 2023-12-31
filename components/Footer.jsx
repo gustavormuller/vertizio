6801d73edbcd51d8d069bab938ddad8a5e39fdca
Gustavo <gusrmuller@gmail.com>
2023-12-30 22:33:20 -0300

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="w-full text-center py-3 border-t border-[#9d67ca]">
+          <p className="text-dimWhite text-[20px]">Copyright © 2024 | Desenvolvido por <Link href="https://www.instagram.com/vertizio_es/" target="_blank">Vertizio</Link></p>      
+      </div>
+    </footer>
   )
 }
 
